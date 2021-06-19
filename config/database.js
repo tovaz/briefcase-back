3473b73d765c339b410eff903b7f98c1189b2dc5
William Valdez <williamvaldez@outlook.com>
2021-06-19 17:37:28 +0200

    fix db

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'mysql',
-        host: env('DATABASE_HOST'),
+        host: env('DATABASE_HOST', 'db_host'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'briefcase'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
+        database: env('DATABASE_NAME', 'db_name'),
+        username: env('DATABASE_USERNAME', 'user'),
+        password: env('DATABASE_PASSWORD', 'password'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
